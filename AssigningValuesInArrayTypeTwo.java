e6fe95098042c9b010b19114beea6f68522e0fa9
Harish Rane <raneharish11@gmail.com>
2023-05-01 22:14:13 +0530

    adding second file

---
+++ AssigningValuesInArrayTypeTwo.java
@@ -0,0 +1,12 @@
+public class AssigningValuesInArrayTypeTwo {
+  
+    public static void main(String args[])
+    {
+        int arr[] = {12,23,45,58};
+
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
